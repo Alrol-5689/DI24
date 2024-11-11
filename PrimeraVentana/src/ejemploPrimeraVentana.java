f9a0a31e4d52f41e6dbb30b2381b0087d39f0281
javiern2023 <154210877+javiern2023@users.noreply.github.com>
2024-11-11 20:02:02 +0100

    Layout y Eventos

--- PrimeraVentana/src/ejemploPrimeraVentana.java
+++ PrimeraVentana/src/ejemploPrimeraVentana.java
@@ -1,19 +1,28 @@
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.JToggleButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ejemploPrimeraVentana extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField tfUsuario;
 	private JPasswordField passwordField;
+	private JToggleButton tgOjo;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -63,5 +72,27 @@ public class ejemploPrimeraVentana extends JFrame {
 		JButton btnNewButton = new JButton("ENTRAR");
 		btnNewButton.setBounds(310, 220, 85, 21);
 		contentPane.add(btnNewButton);
+		
+		tgOjo = new JToggleButton("");
+		tgOjo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (tgOjo.isSelected()) {
+                    // Cambiar a texto visible
+                    passwordField.setEchoChar((char) 0);
+                } else {
+                    // Regresar a texto oculto
+                    passwordField.setEchoChar('*');
+                }
+			}
+		});
+		tgOjo.setIcon(new ImageIcon(ejemploPrimeraVentana.class.getResource("/imagenes/images.png")));
+		tgOjo.setSelected(true);
+		tgOjo.setBounds(261, 141, 25, 18);
+		//tgOjo.setPreferredSize(new Dimension(10, 10));
+		contentPane.add(tgOjo);
+	
+	}
+	public JToggleButton getTgOjo() {
+		return tgOjo;
 	}
 }
